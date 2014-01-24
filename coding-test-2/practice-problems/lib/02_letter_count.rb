4497ae5ab132450a70b56ab8e1c17a07073cf88f
fixxxer1134 <fixxxer7734@gmail.com>
2014-01-24 16:17:56 -0600

    Update 02_letter_count.rb
    there is prolly a slicker way of doing this.

--- coding-test-2/practice-problems/lib/02_letter_count.rb
+++ coding-test-2/practice-problems/lib/02_letter_count.rb
@@ -1,2 +1,13 @@
 def letter_count(str)
+	@str = str.delete(" ").split(//)
+	return_hash = {}
+
+	@str.each do |x|
+		if return_hash.has_key?("#{x}")
+			return_hash["#{x}"] = (return_hash["#{x}"] +1)
+		else return_hash["#{x}"] = 1
+		end
+	end
+
+	return_hash
 end
