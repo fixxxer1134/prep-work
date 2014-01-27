43a91ba4722a211d6d2d00c0c1171c19eba4071d
fixxxer1134 <fixxxer7734@gmail.com>
2014-01-27 15:58:11 -0600

    syncing with my HD

--- coding-test-2/practice-problems/lib/03_ordered_vowels.rb
+++ coding-test-2/practice-problems/lib/03_ordered_vowels.rb
@@ -1,2 +1,28 @@
 def ordered_vowel_words(str)
+	@str = str.split(/ /)
+	return_arr = []
+
+		@str.map do |word|
+			if ordered_vowel_word?(word) then
+				return_arr << word
+			else
+			end
+		end
+		return_arr.join(" ")
+end
+
+def ordered_vowel_word?(word)
+	vowels = []
+	letters = word.split(//)
+
+	letters.each do |x|
+		if x =~ /[aeiou]/
+			vowels << x
+		end
+	end
+
+	if vowels.join == vowels.sort.join
+		return true
+	else return false
+	end
 end
