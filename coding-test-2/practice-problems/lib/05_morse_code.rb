c4a56601159706d1e210f0124714fbaaa7cea531
fixxxer1134 <fixxxer7734@gmail.com>
2014-01-27 18:33:09 -0600

    finished morse_code
    not too bad

--- coding-test-2/practice-problems/lib/05_morse_code.rb
+++ coding-test-2/practice-problems/lib/05_morse_code.rb
@@ -1,2 +1,42 @@
 def morse_encode(str)
+	
+	phrase = str.split(/ /)
+	phrase.map! do |word|
+		word = translate_word(word)
+		word
+	end.join "  "
+
+end
+
+def translate_word(word)
+	hash = {"a" => ".-",
+			"b" => "-...",
+			"c" => "-.-.",
+			"d" => "-..",
+			"e" => ".",
+			"f" => "..-.",
+			"g" => "--.",
+			"h" => "....",
+			"i" => "..",
+			"j" => ".---",
+			"k" => "-.-",
+			"l" => ".-..",
+			"m" => "--",
+			"n" => "-.",
+			"o" => "---",
+			"p" => ".--.",
+			"q" => "--.-",
+			"r" => ".-.",
+			"s" => "...",
+			"t" => "-",
+			"u" => "..-",
+			"v" => "...-",
+			"w" => ".--",
+			"x" => "-..-",
+			"y" => "-.--",
+			"z" => "--.."
+			}
+	word.split(//).map! do |letter|
+		letter = hash[letter]
+		end.join " "
 end
