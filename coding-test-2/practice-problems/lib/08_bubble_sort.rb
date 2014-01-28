3e6528e49bdc93cf9276825650d7607d8dcdd8bd
fixxxer1134 <fixxxer7734@gmail.com>
2014-01-28 15:23:49 -0600

    done with sort
    it's been years since I had to write my own sort algorithm.

--- coding-test-2/practice-problems/lib/08_bubble_sort.rb
+++ coding-test-2/practice-problems/lib/08_bubble_sort.rb
@@ -1,2 +1,18 @@
 def bubble_sort(arr)
+
+	if arr.length <= 1 then return_arr = arr end
+	swapped = true
+	while swapped do
+	    swapped = false
+	    temp = nil
+	    for i in (0..(arr.length - 2)) 
+	       	if arr[i] > arr[i+1]
+	        	temp = arr[i]
+	        	arr[i] = arr[i+1]
+	        	arr[i+1] = temp
+	        	swapped = true
+	     	end
+	    end    
+	end
+  	arr
 end
